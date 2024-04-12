21575299fe50bd4e3c303f1886e672f09d34073e
Phạm Văn Trường <p.v.truong1201@gmail.com>
2024-04-12 22:53:57 +0900

    ooo
    llkl

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 // navar toggle
 const overlay = document.querySelector("[data-overlay]");
 const navOpenBtn = document.querySelector("[data-nav-open-btn]");
@@ -6,33 +5,34 @@ const navbar = document.querySelector("[data-navbar]");
 const navCloseBtn = document.querySelector("[data-nav-close-btn]");
 const navLinks = document.querySelectorAll("[data-nav-link]");
 
+const searchContent = document.querySelector("[data-search-content]");
+const searchOpenBtn = document.querySelector("[data-nav-close-btn]");
+const searchCloseBtn = document.querySelectorAll("[data-nav-link]");
+
 const navElemArr = [navOpenBtn, navCloseBtn, overlay];
 
 const navToggleEvent = function (elem) {
-    for (let i = 0; i < elem.length; i++) {
-        elem[i].addEventListener("click", function () {
-            navbar.classList.toggle("active");
-            overlay.classList.toggle("active");
-        });
-    }
-}
+  for (let i = 0; i < elem.length; i++) {
+    elem[i].addEventListener("click", function () {
+      navbar.classList.toggle("active");
+      overlay.classList.toggle("active");
+    });
+  }
+};
 
 navToggleEvent(navElemArr);
 navToggleEvent(navLinks);
-
 
 // header sticky  & go to top
 const header = document.querySelector("[data-header");
 const goTopBtn = document.querySelector("[data-go-top");
 
-
 window.addEventListener("scroll", function () {
-
-    if (window.scrollY >= 200) {
-        header.classList.add("active");
-        goTopBtn.classList.add("active");
-    } else {
-        header.classList.remove("active");
-        goTopBtn.classList.remove("active");
-    }
+  if (window.scrollY >= 200) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
 });
